7ce466524324a13923fa863a85b8621dcb1a9f5b
Pavel Alekseevich <rangemachine@gmail.com>
2017-06-01 13:20:30 +0300

    Version update

--- Sources/Properties/AssemblyInfo.cs
+++ Sources/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TextureReplacer")]
-[assembly: AssemblyCopyright("© 2013-2016 Davorin Učakar, Ryan Bray, RangeMachine")]
+[assembly: AssemblyCopyright("© 2013-2017 Davorin Učakar, Ryan Bray, RangeMachine")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("2.5.4.0")]
+[assembly: AssemblyVersion("2.6.0.0")]
 
-[assembly: KSPAssembly("Texture Replacer", 2, 5)]
+[assembly: KSPAssembly("Texture Replacer", 2, 6)]
